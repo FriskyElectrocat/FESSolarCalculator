2d8fe4146cbe90b27ad990aff11c52d01a0fab76
Dan Weeks <dan@danimal.org>
2012-03-11 14:58:25 -0700

    add a Mac OS target rename the iOS group in the project navigator

---
+++ SolarCalculatorExample/MacOS_Example/AppDelegate.m
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.m
+//  MacOS_Example
+//
+//  Created by Dan Weeks on 2012-03-11.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+@synthesize window = _window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
---
+++ SolarCalculatorExample/MacOS_Example/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  MacOS_Example
+//
+//  Created by Dan Weeks on 2012-03-11.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
