cf5bf93e7106b2bc95cdf18338dae28b83b4516c
Dan Weeks <dan@danimal.org>
2012-03-12 12:29:41 -0700

    finish adding the OS X example code
    closes #1

--- SolarCalculatorExample/MacOS_Example/AppDelegate.m
+++ SolarCalculatorExample/MacOS_Example/AppDelegate.m
@@ -11,10 +11,59 @@
 @implementation AppDelegate
 
 @synthesize window = _window;
+@synthesize locationLabel;
+@synthesize startDateLabel;
+@synthesize sunriseLabel;
+@synthesize sunsetLabel;
+@synthesize solarNoonLabel;
+@synthesize civilDawnLabel;
+@synthesize civilDuskLabel;
+@synthesize nauticalDawnLabel;
+@synthesize nauticalDuskLabel;
+@synthesize astronomicalDawnLabel;
+@synthesize astronomicalDuskLabel;
+@synthesize solarCalculator=_solarCalculator;
+@synthesize locationManager=_locationManager;
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
     // Insert code here to initialize your application
+    _locationManager = [CLLocationManager new];
+    _locationManager.delegate = self;
+    [self.locationManager startUpdatingLocation];
+}
+
+- (void)locationManager:(CLLocationManager *)manager didUpdateToLocation:(CLLocation *)newLocation fromLocation:(CLLocation *)oldLocation
+{
+    [self.locationManager stopUpdatingLocation];
+    
+    _solarCalculator = [[FESSolarCalculator alloc] initWithDate:[NSDate date] location:newLocation];
+    
+    NSDateFormatter *df = [NSDateFormatter new];
+    df.dateFormat = @"yyyy-MM-dd HH:mm:ss Z";
+    
+    // set the labels
+    self.locationLabel.stringValue = [NSString stringWithFormat:@"Lat: %f  Long: %f", self.solarCalculator.location.coordinate.latitude, self.solarCalculator.location.coordinate.longitude];
+    self.startDateLabel.stringValue = [df stringFromDate:self.solarCalculator.startDate];
+    self.sunriseLabel.stringValue = [df stringFromDate:self.solarCalculator.sunrise];
+    self.sunsetLabel.stringValue = [df stringFromDate:self.solarCalculator.sunset];
+    self.solarNoonLabel.stringValue = [df stringFromDate:self.solarCalculator.solarNoon];
+    
+    self.civilDawnLabel.stringValue = [df stringFromDate:self.solarCalculator.civilDawn];
+    self.civilDuskLabel.stringValue = [df stringFromDate:self.solarCalculator.civilDusk];
+    
+    self.nauticalDawnLabel.stringValue = [df stringFromDate:self.solarCalculator.nauticalDawn];
+    self.nauticalDuskLabel.stringValue = [df stringFromDate:self.solarCalculator.nauticalDusk];
+    
+    self.astronomicalDawnLabel.stringValue = [df stringFromDate:self.solarCalculator.astronomicalDawn];
+    self.astronomicalDuskLabel.stringValue = [df stringFromDate:self.solarCalculator.astronomicalDusk];
+}
+
+- (void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error {
+    
+    [self.locationManager stopUpdatingLocation];
+    
+    NSLog(@"ERROR : %@", error);
 }
 
 @end
