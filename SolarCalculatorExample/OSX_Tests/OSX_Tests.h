a691a8a9b0a7bc75d371a1b80e06427cad8ebf52
Dan Weeks <dan@danimal.org>
2012-03-12 11:48:38 -0700

    add an OS X test target. Uses the same test cases from iOS.

---
+++ SolarCalculatorExample/OSX_Tests/OSX_Tests.h
@@ -0,0 +1,13 @@
+//
+//  OSX_Tests.h
+//  OSX_Tests
+//
+//  Created by Dan Weeks on 2012-03-12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface OSX_Tests : SenTestCase
+
+@end
