28e91ca197f8614f237a1301462d31fcd078d4c3
Dan Weeks <dan@danimal.org>
2013-05-03 22:02:43 -0700

    Add CLLocation category to the project
    - convert from Degrees Minutes Seconds to Decimal Degrees
    - convert from Decimal Degrees to Degrees Minutes Seconds
    - create a CLLocation from Degrees Minutes Seconds
    - add some basic tests

---
+++ SolarCalculatorExample/SolarCalculatorTests/CLLocationFESGraticuleTest.h
@@ -0,0 +1,13 @@
+//
+//  CLLocationFESGraticuleTest.h
+//  SolarCalculatorExample
+//
+//  Created by Dan Weeks on 2013-05-03.
+//
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface CLLocationFESGraticuleTest : SenTestCase
+
+@end
